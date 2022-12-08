10c1a7fd6477e040ab521d7bb6e3e6c553764549
RTPritee <pritee593@gmail.com>
2022-12-08 17:34:15 +0600

    second commit

--- djangoForm/members/views.py
+++ djangoForm/members/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello world!")
-    return render(request,"index.html")
+    # return render(request,"index.html")
+    return render(request,"complainForm.html")
